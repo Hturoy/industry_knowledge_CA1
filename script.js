8c35f433df6118211e5c9f2a6edd0353cc196bad
Hturoy <69842773+Hturoy@users.noreply.github.com>
2022-01-20 15:54:02 +0100

    added icons + started burgermenu
    added some icons, and made the burger menu function. some css remaining

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuIcon = document.querySelector('.menuIcon')
+const closeIcon = document.querySelector('.closeIcon')
+const menuLinks = document.querySelector('.wrapperBurgerLink')
+
+
+
+function openMenu(){
+    closeIcon.style.display = 'inline'
+    menuIcon.style.display = 'none'
+    menuLinks.style.display = 'flex'
+}
+
+function closeMenu(){
+    closeIcon.style.display = 'none'
+    menuIcon.style.display = 'inline'
+    menuLinks.style.display = 'none'
+
+}
